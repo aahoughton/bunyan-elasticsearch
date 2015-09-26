d842032542c436362da63d10688a7a0fcc5f4f76
Andrew Houghton <andrew@electricimp.com>
2015-09-26 15:59:09 -0400

    add level mappings from options

--- index.js
+++ index.js
@@ -4,14 +4,29 @@ var util = require('util');
 var elasticsearch = require('elasticsearch');
 var moment = require('moment');
 
-var levels = {
-  10: 'trace',
-  20: 'debug',
-  30: 'info',
-  40: 'warn',
-  50: 'error',
-  60: 'fatal'
-};
+// we assume unknown levels should get mapped to the next
+// highest known level name
+function getLevelName(levels, level) {
+  var levelName = levels[level];
+
+  if (!levelName) {
+    if (level > 50) {
+      levelName = 'fatal';
+    } else if (level > 40) {
+      levelName = 'error';
+    } else if (level > 30) {
+      levelName = 'warn';
+    } else if (level > 20) {
+      levelName = 'info';
+    } else if (level > 10) {
+      levelName = 'debug';
+    } else {
+      levelName = 'trace';
+    }
+  }
+
+  return levelName;
+}
 
 function generateIndexName (pattern, entry) {
   return moment.utc(entry.timestamp).format(pattern);
@@ -26,10 +41,11 @@ function callOrString (value, entry) {
 
 function ElasticsearchStream (options) {
   options = options || {};
+  this._levels = options.levels || {};
   this._client = options.client || new elasticsearch.Client(options);
   this._type = options.type || 'logs';
   var indexPattern = options.indexPattern || '[logstash-]YYYY.MM.DD';
-  this._index = options.index || generateIndexName.bind(null, indexPattern); 
+  this._index = options.index || generateIndexName.bind(null, indexPattern);
   Writable.call(this, options);
 }
 
@@ -40,19 +56,20 @@ ElasticsearchStream.prototype._write = function (entry, encoding, callback) {
   var client = this._client;
   var index = this._index;
   var type = this._type;
+  var levels = this._levels;
 
   var d = domain.create();
-  d.on('error', function (err) { 
+  d.on('error', function (err) {
     console.log("Elasticsearch Error", err.stack);
   });
   d.run(function () {
     entry = JSON.parse(entry.toString('utf8'));
     var env = process.env.NODE_ENV || 'development';
 
-    // Reassign these fields so them match what the default Kibana dashboard 
+    // Reassign these fields so them match what the default Kibana dashboard
     // expects to see.
     entry['@timestamp'] = entry.time;
-    entry.level = levels[entry.level];
+    entry.level = getLevelName(levels, entry.level);
     entry.message = entry.msg;
 
     // remove duplicate fields
